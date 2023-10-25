636cb4c2197689c6b0afd47023509ac3405a0f36
heerat-singh <heeratsingh54@gmail.com>
2023-10-25 10:35:02 -0400

    NavBar implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Navigation from "./Customer/Components/NavBar/Navigation";
 
 function App() {
   return (
-    <div className="App">
+    <div className="">
+        <Navigation/>
+
      E-commerce
     </div>
   );
